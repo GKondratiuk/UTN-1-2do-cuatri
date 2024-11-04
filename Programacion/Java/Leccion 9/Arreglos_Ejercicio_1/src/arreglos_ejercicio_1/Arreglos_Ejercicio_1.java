1b11008ceb2c828f80163098f56511af76b5ec6e
guille <kondra.guille@gmail.com>
2024-11-04 13:02:43 -0300

    hasta clase 11 pt2 video 5

--- Programacion/Java/Leccion 9/Arreglos_Ejercicio_1/src/arreglos_ejercicio_1/Arreglos_Ejercicio_1.java
+++ Programacion/Java/Leccion 9/Arreglos_Ejercicio_1/src/arreglos_ejercicio_1/Arreglos_Ejercicio_1.java
@@ -4,12 +4,17 @@ public class Arreglos_Ejercicio_1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        float[] arreglo = new float[5];
+        float[] arreglos = new float[5];
         System.out.println("Guardando los datos en el arreglo");
         for (int i = 0; i < 5; i++) {
             System.out.println((i+1) + ". Digite un numero: ");
-            arreglo[i] = entrada.nextFloat();
+            arreglos[i] = entrada.nextFloat();
             //falta video 7 de la clase 11 pt 1
         }
+        System.out.println("\nImprimir los elementos del arreglo");
+        for(float i: arreglos){
+            System.out.println(i + " ");
+        }
+        System.out.println("\n");
     }
 }
